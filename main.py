a21697aca048b0db77230a81772bc8e394e7f1e8
Ivan Novikov <johnnovikov0@gmail.com>
2017-12-31 22:14:35 +0300

    backend almost ready

--- main.py
+++ main.py
@@ -1,11 +1,37 @@
-from flask import Flask, config
+from flask import Flask, config, render_template, request
+import redis
+import re
 
 app = Flask(__name__)
+red = redis.StrictRedis(host='localhost', port=6379, db=0)
+app.config.from_object('config')
+
+def match_string(string):
+    return re.match(r"^HappyNewYearOurDearStudentsWeLoveYou.*",string)
+
+
+@app.route('/new',methods=["POST"])
+def new():
+    text = request.values.get("text")
+    if not text:
+        return "Ну напиши что-нибудь уже!"
+    if match_string(text):
+        return app.config["FLAG"]
+    red.lpush('new_year',text)
+    return "Спасибо за поздравление. Теперь оно доступно на страничке /list"
+
+
+
+
+@app.route('/list')
+def list_all():
+    data = red.lrange("new_year", 0, -1)
+    return render_template("list.html", items=data)
 
 
 @app.route('/')
 def hello_world():
-    return
+    return render_template('index.html')
 
 
 if __name__ == '__main__':
