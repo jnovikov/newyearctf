f37088c5e19646ce0bd5fddb7f79b71d4e3f53b1
Ivan Novikov <johnnovikov0@gmail.com>
2017-12-31 22:23:51 +0300

    Backend ready :)

--- main.py
+++ main.py
@@ -6,26 +6,27 @@ app = Flask(__name__)
 red = redis.StrictRedis(host='localhost', port=6379, db=0)
 app.config.from_object('config')
 
+
 def match_string(string):
-    return re.match(r"^HappyNewYearOurDearStudentsWeLoveYou.*",string)
+    return re.match(r"^HappyNewYearOurDearStudentsWeLoveYou.*", string)
 
 
-@app.route('/new',methods=["POST"])
+@app.route('/new', methods=["POST", "GET"])
 def new():
-    text = request.values.get("text")
-    if not text:
-        return "Ну напиши что-нибудь уже!"
-    if match_string(text):
-        return app.config["FLAG"]
-    red.lpush('new_year',text)
-    return "Спасибо за поздравление. Теперь оно доступно на страничке /list"
-
-
+    if request.method == "POST":
+        text = request.values.get("text")
+        if not text:
+            return "Ну напиши что-нибудь уже!"
+        if match_string(text):
+            return app.config["FLAG"]
+        red.lpush('new_year', text)
+        return "Спасибо за поздравление. Теперь оно доступно на страничке /list"
+    return render_template('new.html')
 
 
 @app.route('/list')
 def list_all():
-    data = red.lrange("new_year", 0, -1)
+    data = [x.decode() for x in red.lrange("new_year", 0, -1)]
     return render_template("list.html", items=data)
 
 
